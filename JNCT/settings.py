1ee9f077ef380753508c80eb8d39a8a0608e965e
ampyy <49230927+ampyy@users.noreply.github.com>
2021-09-26 20:10:25 +0530

    Update settings.py

--- JNCT/settings.py
+++ JNCT/settings.py
@@ -125,3 +125,20 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
